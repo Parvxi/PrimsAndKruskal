e8b40b0b6cb17d37c673b9eba4aecc93304a25e2
Amina122019 <97847067+Amina122019@users.noreply.github.com>
2023-05-19 14:55:10 +0000

    Update CPCS324_ProjectP1.java

--- CPCS324_ProjectP1.java
+++ CPCS324_ProjectP1.java
@@ -15,17 +15,27 @@ public class CPCS324_ProjectP1 {
  
     public static void main(String[] args) throws FileNotFoundException {
         
+        
         Graph graph = new Graph();
-        String fileName = "sampleInput.txt";
+        String fileName;
+        fileName = "C:\\Users\\Desktop\\PROJECTTEXT.txt\\";
         graph.readGraphFromFile(fileName);
-     
-        Scanner s = new Scanner(fileName);
-        
-    
-              
-        
+   Scanner sc= new Scanner(System.in);
     }
-}
+    {
+          
+    
+                Scanner sc= new Scanner(System.in);
+		System.out.println("enter the number of vertex");
+		int n=sc.nextInt(); // no of nodes in a graph
+		System.out.println("enter the no of edges ");
+		int m=sc.nextInt(); // no of edges in a graph
+		int i=0;
+		
+               Graph obj = new Graph();
+               obj.makeGraph(n, m, false);
+	}
+    }
 
     
 
